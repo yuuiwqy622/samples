5952961fe08f275993b74ab1b619dbc9b3eaa91f
yuuiwqy622 <random064@gmail.com>
2020-10-30 14:47:41 +0100

    selection of interval and current value

--- sm.js
+++ sm.js
@@ -1,12 +1,13 @@
-function selectTd(e) {
+function addCssClass(e, css) {
     e.preventDefault()
     let l = e.target.classList
-    if (l.contains('selected')) {
-        l.remove('selected')
+    if (l.contains(css)) {
+        l.remove(css)
         return
     }
-    l.add('selected')
+    l.add(css)
 }
+
 
 function createTable(lines) {
     let n = parseInt(lines[0])
@@ -18,23 +19,36 @@ function createTable(lines) {
     td = document.createElement('td')
     td.textContent = 'v'
     trv.append(td)
+    let trs = document.createElement('tr')
+    td = document.createElement('td')
+    td.textContent = 's'
+    trs.append(td)
 
     for (let i = 1; i <= n; ++i) {
         let [t, v] = lines[i].split(' ')
         td = document.createElement('td')
-        td.addEventListener('contextmenu', selectTd)
+        td.addEventListener('contextmenu', (e) => addCssClass(e, 'selected'))
+        td.addEventListener('wheel', (e) => addCssClass(e, 'interval'))
         td.textContent = t
         trt.append(td)
 
         td = document.createElement('td')
-        td.addEventListener('contextmenu', selectTd)
+        td.addEventListener('contextmenu', (e) => addCssClass(e, 'selected'))
+        td.addEventListener('wheel', (e) => addCssClass(e, 'interval'))
         td.textContent = v
         trv.append(td)
+
+        td = document.createElement('td')
+        td.addEventListener('contextmenu', (e) => addCssClass(e, 'selected'))
+        td.addEventListener('wheel', (e) => addCssClass(e, 'interval'))
+        td.textContent = i < 2 ? v : parseInt(v) + parseInt(trs.lastChild.textContent)
+        trs.append(td)
     }
 
     let t = document.querySelector('table')
     t.append(trt)
     t.append(trv)
+    t.append(trs)
 }
 
 function readFile(e) {
