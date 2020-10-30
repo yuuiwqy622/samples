9977150e4416a99c96618d5aa286aea2ec80d56f
yuuiwqy622 <random064@gmail.com>
2020-10-30 14:47:02 +0100

    average

--- samples.cpp
+++ samples.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <iostream>
-#include <stdio.h>
+#include <set>
 #include <string>
 
 #define AVG 0
@@ -10,20 +10,19 @@
 
 using namespace std;
 
-int n, v[100000], t[100000];
+int n, v[100000], t[100000], s[10000];
 
 int avg(bool lt, int invl) {
   int res = 0;
-  double sum = 0;
-  int cnt, *l = t, *pl = t;
+  double sum;
+  int cnt;
 
-  for (int i = 0; i < n; ++i) {
-    l = lower_bound(t, t + i, t[i] - invl);
-    cnt = &t[i] - l;
+  for (int i = 1; i < n; ++i) {
+    int idx = lower_bound(t, t + i, t[i] - invl) - t;
+    cnt = i - idx;
     if (!cnt)
       continue;
-    if (l > pl)
-      ;
+    sum = idx ? s[i - 1] - s[idx - 1] : s[i - 1];
 
     res += lt && v[i] < sum / cnt;
     res += !lt && v[i] > sum / cnt;
@@ -32,17 +31,31 @@ int avg(bool lt, int invl) {
   return res;
 }
 
-int maxS(bool lt, int invl) { return 1; }
+/*
+ * 21. binary search to find start of interval is log(n), which is better than
+ * linear search from previous start of interval or new end of interval
+ * 22. how many items are in interval
+ * 25. if interval's start moved to right, recalculate sum
+ */
 
-int minS(bool lt, int invl) { return 1; }
+int maxS(bool lt, int invl) { return 2; }
+
+int minS(bool lt, int invl) {
+  int res = 0;
+  multiset<int> vals;
+
+  return res;
+}
 
 int main(void) {
   scanf("%d", &n);
-  for (int i = 0; i < n; ++i)
+  for (int i = 0; i < n; ++i) {
     scanf("%d %d", &t[i], &v[i]);
+    s[i] = i ? s[i - 1] + v[i] : v[i];
+  }
 
   int c;
-  scanf("%d", &c);
+  scanf("%d\n", &c);
   while (c--) {
     int fun, invl;
 
@@ -51,7 +64,7 @@ int main(void) {
     bool lt = s[0] == 'l';
     sscanf(s.c_str() + 7, "%d", &invl);
 
-    int res;
+    int res = 0;
     if (s[4] == 'v')
       res = avg(lt, invl);
     else if (s[4] == 'a')
